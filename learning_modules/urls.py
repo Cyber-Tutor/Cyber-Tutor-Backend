47d05271fdee4af37a45a346adb15d4ef4f90eb0
Dallas LaComb <dallaslacombdrive@gmail.com>
2023-12-31 14:03:39 -0500

    Remove unused import and empty line in urls.py, and remove CHATGPT_API_KEY from settings.py

--- learning_modules/urls.py
+++ learning_modules/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('content/', views.content, name='content'),
-
 ]
--- research_project/settings.py
+++ research_project/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
-CHATGPT_API_KEY = os.environ['CHATGPT_API_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
