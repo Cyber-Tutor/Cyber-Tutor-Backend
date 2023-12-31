b660f4c26611b2508bc10b91d0c4cfbeea4c1301
Matthew Quijano <mq022002@gmail.com>
2023-12-31 03:23:04 -0500

    Change: Defined crispy-forms and crispy-bootstrap5

--- research_project/settings.py
+++ research_project/settings.py
@@ -38,13 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_browser_reload",
+    
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'django_browser_reload',
+    
     'admin_tools',
     'assessments',
     'learning_analytics',
     'learning_modules',
     'user_profiles',
     ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
