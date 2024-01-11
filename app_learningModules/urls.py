9168391de6f7e6edfd780a4c450ebecb2ee31653
Matthew Quijano <mq022002@gmail.com>
2024-01-11 07:13:42 -0500

    Add: Learning path views and urlpatterns
    - course_list: Simply grabs all courses and passes it to the course_list template
    - get_modules_for_course: Grabs modules associated with specified course. Then it converts it to a list of dictionaries, then passed to client as a JSON response that we can use in our JavaScript.
    - module_detail: Grabs the course, and module then passes it to the module_detail template.

--- app_learningModules/urls.py
+++ app_learningModules/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('content/', views.content, name='content'),
+    path('courses/', views.course_list, name='course_list'),
+    path('courses/<int:course_id>/modules/', views.get_modules_for_course, name='get_modules_for_course'),
+    path('courses/<int:course_id>/module/<int:module_number>/', views.module_detail, name='module_detail'),
+
 ]
--- app_learningModules/views.py
+++ app_learningModules/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Course, Module
 import os 
 from openai import OpenAI
+
 CHATGPT_API_KEY = os.environ['CHATGPT_API_KEY']
 
 client  = OpenAI(api_key=CHATGPT_API_KEY)
@@ -21,3 +24,21 @@ def content(request):
             except Exception as e:  # Catch any exceptions from the API call
                 result = f"An error occurred: {str(e)}"
     return render(request, 'content.html', {'result': result})
+
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'course_list.html', {'courses': courses})
+
+# Grabs modules associated with specified course. Then it converts it to a list 
+# of dictionaries, then passed to client as a JSON response that we can use in 
+# our JavaScript.
+def get_modules_for_course(request, course_id):
+    modules = Module.objects.filter(course_id=course_id).order_by('number')
+    modules_data = list(modules.values('id', 'title', 'description'))
+    return JsonResponse(modules_data, safe=False)
+
+def module_detail(request, course_id, module_number):
+    course = get_object_or_404(Course, id=course_id)
+    module = get_object_or_404(Module, course=course, number=module_number)
+    return render(request, 'module_detail.html', {'course': course, 'module': module})
+
