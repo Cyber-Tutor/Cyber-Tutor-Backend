7da2a92b24ef3e4da1885ae48df08872ffb138be
Matthew Quijano <mq022002@gmail.com>
2024-01-17 00:16:50 -0500

    Remove: openai package and dependencies; ChatGPT integration

--- app_learningModules/views.py
+++ app_learningModules/views.py
@@ -2,28 +2,6 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Course, Module
 import os 
-from openai import OpenAI
-
-CHATGPT_API_KEY = os.environ['CHATGPT_API_KEY']
-
-client  = OpenAI(api_key=CHATGPT_API_KEY)
-
-def content(request):
-    result = ''
-    if request.method == "POST":
-        question = request.POST.get('question')
-        if question:
-            try:
-                # Use the client to create a chat completion
-                response = client.chat.completions.create(
-                    model="gpt-3.5-turbo",  # Make sure to use the correct chat model
-                    messages=[{"role": "user", "content": question}]
-                )
-                # Access the response attributes
-                result = response.choices[0].message.content
-            except Exception as e:  # Catch any exceptions from the API call
-                result = f"An error occurred: {str(e)}"
-    return render(request, 'content.html', {'result': result})
 
 def course_list(request):
     courses = Course.objects.all()
