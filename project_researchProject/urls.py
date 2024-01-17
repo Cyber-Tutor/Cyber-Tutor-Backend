a9089261255a074b2dd2bbfce7777cad4c69e19e
Matthew Quijano <mq022002@gmail.com>
2024-01-16 23:18:52 -0500

    Fix: Forgot to add app_adminDashboard URLs to root urls.py

--- project_researchProject/urls.py
+++ project_researchProject/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path('', include('app_adminDashboard.urls')),
     path('', include('app_learningModules.urls')),
     path('', include('app_users.urls')),
 ]
