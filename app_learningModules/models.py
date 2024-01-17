50a9e12884afb15e4a988a537d700b55dc124b23
Matthew Quijano <mq022002@gmail.com>
2024-01-16 23:05:22 -0500

    Change: Module model to reflect planned changes

--- app_learningModules/models.py
+++ app_learningModules/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
+
 class Course(models.Model):
     title = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
 
+
 class Module(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    order = models.PositiveIntegerField(null=True, blank=True)
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
+    prompt = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
