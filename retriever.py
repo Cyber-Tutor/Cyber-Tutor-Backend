862ee01962f0ebe18613d8bcda8fcfd630923f52
Sean Clifford <seancliff01@gmail.com>
2024-02-04 14:15:54 -0500

    saves split docs to chroma db database

--- retriever.py
+++ retriever.py
@@ -20,16 +20,13 @@ loader = WebBaseLoader([
 ])
 documents = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
+docs = text_splitter.split_documents(documents)
 
-print(len(texts))
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", convert_system_message_to_human=True)
 
-db = Chroma.from_documents(texts, embeddings, collection_name="cyber-tutor")
+db = Chroma.from_documents(docs, embeddings, persist_directory="./chroma_db")
 retriever = db.as_retriever()
-
-db.persist()
 
 llm = ChatGoogleGenerativeAI(model="models/gemini-pro", convert_system_message_to_human=True)
 
