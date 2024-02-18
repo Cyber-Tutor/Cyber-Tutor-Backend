fc65cf9a9dc6898dd6bdd275c2ec1da62cfcbb5e
Sean Clifford <seancliff01@gmail.com>
2024-02-18 15:01:19 -0500

    added check quiz answers, save quiz scores, and updated get quiz/test questions

--- firebase.py
+++ firebase.py
@@ -1,5 +1,8 @@
 import pyrebase
+import dotenv
 import os
+
+dotenv.load_dotenv()
 
 class Firebase:
     def __init__(self):
@@ -19,17 +22,25 @@ class Firebase:
     # get user group (experimental or control)
     # TODO: implement user group
     def user_group(self, user_id):
-        group = self.db.child("users").child(user_id).child("group").get()
+        group = self.db.child("users").child(user_id).child("group").get().val()
         return group
     
     # returns all quiz questions in given section/chapter/group
     def get_quiz_questions(self, section, chapter, group):
-        questions = self.db.child("topics").child(section).child(chapter).child("quiz").child(group).get().val()
+        question_ids = self.db.child("topics").child(section).child(chapter).child("quiz").child(group).get().val()
+        questions = []
+        for question_id in question_ids:
+            question = self.db.child("questions").child("quiz").child(question_id).get().val()
+            questions.append(question)
         return questions
     
     # returns all test questions in given section/chapter/group
-    def get_test_questions(self, section, chapter, group):
-        questions = self.db.child("topics").child(section).child(chapter).child("test").child(group).get().val()
+    def get_test_questions(self, section, group):
+        question_ids = self.db.child("topics").child(section).child("test").child(group).get().val()
+        questions = []
+        for question_id in question_ids:
+            question = self.db.child("questions").child("test").child(question_id).get().val()
+            questions.append(question)
         return questions
     
     # gets reading content for given section/chapter/group
@@ -37,3 +48,13 @@ class Firebase:
         reading = self.db.child("topics").child(section).child(chapter).child(group).get().val()
         return reading['chapterContent']
     
+    # check answer to quiz question
+    def check_quiz_answer(self, question_id, answer):
+        question = self.db.child("questions").child("quiz").child(question_id).get().val()
+        return question['answer'] == answer
+    
+    # save quiz results
+    def save_quiz_score(self, user_id, section, chapter, score):
+        self.db.child("users").child(user_id).child("scores").child("quiz").child(section).child(chapter).set({
+            "score": score,
+        })
