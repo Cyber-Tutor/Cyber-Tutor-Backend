bfc7bfa119d9da86b4ce07f08c7c98fa6a0afec7
Sean Clifford <seancliff01@gmail.com>
2024-02-25 15:31:15 -0500

    saves output to json, tests llm output if valid json

--- content_generation/quiz_creator.py
+++ content_generation/quiz_creator.py
@@ -1,31 +1,44 @@
 from question_generator import QuestionGenerator
 from firebase import Firebase
+import json
 
 import argparse
 
 
-def quiz_creator(section, chapter, details_path, difficulty, topic):
-    if not topic:
-        topic = section
-
-    db = Firebase()
+def quiz_creator(details_path, difficulty, topic, q_per_detail=1):
+    #db = Firebase()
     q_gen = QuestionGenerator()
     # get list of question details from file
     details = get_details(details_path)
+    quiz = []
 
     # create questions for each detail based on topic, difficulty
     for detail in details:
-        question = q_gen.create_question(topic, detail, difficulty)
-        quiz_question = {
-            "question": question,
-            "difficulty": difficulty,
-        }
-        db.create_question(section, chapter, quiz_question)
+        for _ in range(q_per_detail):
+            question = q_gen.create_question(topic, detail, difficulty)
+            
+            # test to see if llm output is valid json 
+            try: 
+                json.loads(question)
+            except json.JSONDecodeError:
+                continue
+
+            quiz_question = {
+                "question": question,
+                "difficulty": difficulty,
+            }
+            #db.create_question(section, chapter, quiz_question)
+            quiz.append(quiz_question)
+
+    return quiz
 
 def get_details(details_path):
     with open(details_path, 'r') as f:
         return f.readlines()
 
+def save_quiz(quiz, save_path):
+    with open(save_path, 'w') as f:
+        json.dump(quiz, f)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -34,7 +47,10 @@ if __name__ == '__main__':
     parser.add_argument('--details_path', type=str, help='a string for the question topics path')
     parser.add_argument('--difficulty', type=str, help='a string for the difficulty')
     parser.add_argument('--topic', type=str, help='a string for the topic')
+    parser.add_argument('--q_per_detail', type=int, help='an integer for the number of questions to generate per detail', default=1)
+    parser.add_argument('--save_path', type=str, help='a string for the save path')
 
     args = parser.parse_args()
 
-    quiz_creator(args.section, args.chapter, args.details_path, args.difficulty)
+    quiz = quiz_creator(args.details_path, args.difficulty, args.topic, args.q_per_detail)
+    save_quiz(quiz, args.save_path)
