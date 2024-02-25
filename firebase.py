cb4989e5e94394f7d2e57b9d473cb11e7a96e85c
Sean Clifford <seancliff01@gmail.com>
2024-02-25 16:40:03 -0500

    updated (almost) all methods to work with firestore and new structure

--- firebase.py
+++ firebase.py
@@ -2,6 +2,10 @@ import pyrebase
 import dotenv
 import os
 
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
 dotenv.load_dotenv()
 
 
@@ -10,6 +14,7 @@ Firebase class to interact with Firebase database
 """
 class Firebase:
     def __init__(self):
+        """
         self.firebase_config = {
             "apiKey": os.environ['FIREBASE_API_KEY'],
             "authDomain": os.environ['FIREBASE_AUTH_DOMAIN'],
@@ -19,9 +24,15 @@ class Firebase:
             "messagingSenderId": os.environ['FIREBASE_MESSAGING_SENDER_ID'],
             "appId": os.environ['FIREBASE_APP_ID'],
         }
+        """
+        self.cred = credentials.Certificate(os.environ['FIREBASE_CRED_PATH'])
+        self.app = firebase_admin.initialize_app()
+        self.db = firestore.client()
 
+        """
         self.firebase = pyrebase.initialize_app(self.firebase_config)
         self.db = self.firebase.database()
+        """
 
     # get user group (experimental or control)
     # TODO: implement user group
@@ -31,32 +42,64 @@ class Firebase:
     
     # returns all quiz questions in given section/chapter/group
     def get_quiz_questions(self, section, chapter, group):
+        questions_ref = self.db.collection('quizQuestions').document(section).collection(group).document(chapter).collection('questions')
+        questions = questions_ref.get()
+        return questions
+        """
         question_ids = self.db.child("topics").child(section).child(chapter).child("quiz").child(group).get().val()
         questions = []
         for question_id in question_ids:
             question = self.db.child("questions").child("quiz").child(question_id).get().val()
             questions.append(question)
         return questions
+        """
     
     # returns all test questions in given section/chapter/group
     def get_test_questions(self, section, group):
+        questions_ref = self.db.collection('testQuestions').document(section).collection(group)
+        questions = questions_ref.get()
+        return questions
+        """
         question_ids = self.db.child("topics").child(section).child("test").child(group).get().val()
         questions = []
         for question_id in question_ids:
             question = self.db.child("questions").child("test").child(question_id).get().val()
             questions.append(question)
         return questions
+        """
     
     # gets reading content for given section/chapter/group
     def get_reading(self, section, chapter, group):
+        reading_ref = self.db.collection('topics').document(section).collection('chapters').document(chapter)
+        reading = reading_ref.get()
+        content = {
+            "title": reading['chapterTitle'],
+            "description": reading['chapterDescription'],
+            "type": reading['chapterType']
+        }
+        if group == 'experimentalGroup':
+            content['content'] = reading['experimentalGroupContent']
+            content['images'] = reading['experimentaalGroupImageURLs']
+            return content
+        else:
+            content['content'] = reading['controlGroupContent']
+            content['images'] = reading['controlGroupImageURLs']
+            return content
+        """
         reading = self.db.child("topics").child(section).child(chapter).child(group).get().val()
         return reading['chapterContent']
+        """
     
     # check answer to quiz question
     # TODO: implement quiz pool
-    def check_quiz_answer(self, question_id, answer):
+    def check_quiz_answer(self, section, chapter, group, question_id, answer):
+        question_ref = self.db.collection('quizQuestions').document(section).collection(group).document(chapter).collection('questions').document(question_id)
+        question = question_ref.get()
+        return question['correctAnswer'] == answer
+        """
         question = self.db.child("questions").child("quiz").child(question_id).get().val()
         return question['answer'] == answer
+        """
     
     # save quiz results
     # TODO: implement user scores
@@ -66,10 +109,14 @@ class Firebase:
         })
 
     # check answer to test question
-    def check_test_answer(self, question_id, answer):
+    def check_test_answer(self, section, group, question_id, answer):
+        question_ref = self.db.collection('testQuestions').document(section).collection(group).document(question_id)
+        question = question_ref.get()
+        return question['correctAnswer'] == answer
+        """
         question = self.db.child("questions").child("test").child(question_id).get().val()
         return question['answer'] == answer
-    
+        """
     # save test results
     def save_test_score(self, user_id, section, score):
         self.db.child("users").child(user_id).child("scores").child("test").child(section).set({
