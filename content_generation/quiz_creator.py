5617bda701a16ebb56df57247aae361edb81acd1
Sean Clifford <seancliff01@gmail.com>
2024-03-03 15:41:47 -0500

    fixed json output and changed arguments

--- content_generation/quiz_creator.py
+++ content_generation/quiz_creator.py
@@ -1,5 +1,5 @@
 from question_generator import QuestionGenerator
-from firebase import Firebase
+#from firebase import Firebase
 import json
 
 import argparse
@@ -13,22 +13,26 @@ def quiz_creator(details_path, difficulty, topic, q_per_detail=1):
     quiz = []
 
     # create questions for each detail based on topic, difficulty
-    for detail in details:
-        for _ in range(q_per_detail):
-            question = q_gen.create_question(topic, detail, difficulty)
-            
-            # test to see if llm output is valid json 
-            try: 
-                json.loads(question)
-            except json.JSONDecodeError:
-                continue
+    for level in difficulty:
+        for detail in details:
+            for _ in range(q_per_detail):
+                # pass if question generation fails
+                try:
+                    question = q_gen.create_question(topic, detail, level)
+                except:
+                    continue
+                # test to see if llm output is valid json 
+                try: 
+                    json.loads(question)
+                except json.JSONDecodeError:
+                    continue
 
-            quiz_question = {
-                "question": question,
-                "difficulty": difficulty,
-            }
-            #db.create_question(section, chapter, quiz_question)
-            quiz.append(quiz_question)
+                quiz_question = {
+                    "question": json.loads(question),
+                    "difficulty": level,
+                }
+                #db.create_question(section, chapter, quiz_question)
+                quiz.append(quiz_question)
 
     return quiz
 
@@ -45,7 +49,7 @@ if __name__ == '__main__':
     parser.add_argument('--section', type=int, help='an integer for the section')
     parser.add_argument('--chapter', type=int, help='an integer for the chapter')
     parser.add_argument('--details_path', type=str, help='a string for the question topics path')
-    parser.add_argument('--difficulty', type=str, help='a string for the difficulty')
+    parser.add_argument('--difficulty', nargs='+', help='a list of strings for the difficulty levels', default=['beginner', 'intermediate', 'hard'])
     parser.add_argument('--topic', type=str, help='a string for the topic')
     parser.add_argument('--q_per_detail', type=int, help='an integer for the number of questions to generate per detail', default=1)
     parser.add_argument('--save_path', type=str, help='a string for the save path')
