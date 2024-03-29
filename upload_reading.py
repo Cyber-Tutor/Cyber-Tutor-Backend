beea232cfcdc0624f86d75b40a38c7b1587f7c08
Sean Clifford <seancliff01@gmail.com>
2024-03-29 15:46:57 -0400

    fixed messed up arguments

--- upload_reading.py
+++ upload_reading.py
@@ -11,10 +11,10 @@ def upload_reading(section, chapter, reading_path, group):
             if file.endswith(".txt"):
                 reading_file_path = os.path.join(reading_path, file)
                 reading = load_reading(reading_file_path)
-                db.store_reading(section, chapter, reading, group)
+                db.create_reading(section, chapter, group, reading)
     else:
         reading = load_reading(reading_path)
-        db.store_reading(section, chapter, reading, group)
+        db.create_reading(section, chapter, group, reading)
 
 
 def load_reading(reading_path):
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Store reading into firebase')
     parser.add_argument('--reading_path', type=str, help='reading path for a file or directory containing readings', required=True)
     parser.add_argument('--group', type=str, help='group for the reading', required=True)
-    parser.add_argument('--section', type=int, help='section for the reading', required=True)
-    parser.add_argument('--chapter', type=int, help='chapter for the reading', required=True)
+    parser.add_argument('--section', type=str, help='section for the reading', required=True)
+    parser.add_argument('--chapter', type=str, help='chapter for the reading', required=True)
     args = parser.parse_args()
 
     upload_reading(args.section, args.chapter, args.reading_path, args.group)
