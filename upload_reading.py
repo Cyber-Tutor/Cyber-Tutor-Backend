4035a23a818f6ec57d16d0e163d2305fd3779e24
Sean Clifford <seancliff01@gmail.com>
2024-04-01 13:52:27 -0400

    updated for uploading reading based on difficulties

--- upload_reading.py
+++ upload_reading.py
@@ -9,12 +9,18 @@ def upload_reading(section, chapter, reading_path, group):
     if os.path.isdir(reading_path):
         for file in os.listdir(reading_path):
             if file.endswith(".txt"):
-                reading_file_path = os.path.join(reading_path, file)
-                reading = load_reading(reading_file_path)
-                db.create_reading(section, chapter, group, reading)
+                # get difficulty for each reading file from file name
+                difficulty = "beginner" if "beginner" in file else "intermediate" if "intermediate" in file else "expert" if "expert" in file else None
+                if difficulty:
+                    reading_file_path = os.path.join(reading_path, file)
+                    reading = load_reading(reading_file_path)
+                    db.create_reading(section, chapter, group, reading, difficulty)
     else:
         reading = load_reading(reading_path)
-        db.create_reading(section, chapter, group, reading)
+        file = os.path.basename(reading_path)
+        difficulty = "beginner" if "beginner" in file else "intermediate" if "intermediate" in file else "expert" if "expert" in file else None
+        if difficulty:
+            db.create_reading(section, chapter, group, reading)
 
 
 def load_reading(reading_path):
@@ -29,9 +35,11 @@ if __name__ == '__main__':
     """
     parser = argparse.ArgumentParser(description='Store reading into firebase')
     parser.add_argument('--reading_path', type=str, help='reading path for a file or directory containing readings', required=True)
-    parser.add_argument('--group', type=str, help='group for the reading', required=True)
     parser.add_argument('--section', type=str, help='section for the reading', required=True)
     parser.add_argument('--chapter', type=str, help='chapter for the reading', required=True)
+    parser.add_argument('--group', type=str, help='group for the reading', default='experimental')
+    # optional arguments
     args = parser.parse_args()
 
     upload_reading(args.section, args.chapter, args.reading_path, args.group)
+    # python upload_reading.py --reading_path content_generation/content/reading/2fa --section two_factor_authentication --chapter introduction_to_2fa
