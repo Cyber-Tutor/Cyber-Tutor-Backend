633f2c4d1267f89446ec265c3e7a0165b66f2439
Sean Clifford <seancliff01@gmail.com>
2024-04-02 18:02:47 -0400

    added completed print out

--- upload_reading.py
+++ upload_reading.py
@@ -20,6 +20,7 @@ def upload_reading(section, chapter, reading_path, group):
                     reading_file_path = os.path.join(reading_path, file)
                     reading = load_reading(reading_file_path)
                     db.create_reading(section, chapter, group, reading, difficulty)
+                    print("Reading uploaded for", file)
     else:
         # if reading path is a file, upload the reading file
         reading = load_reading(reading_path)
@@ -27,7 +28,7 @@ def upload_reading(section, chapter, reading_path, group):
         difficulty = "beginner" if "beginner" in file else "intermediate" if "intermediate" in file else "expert" if "expert" in file else None
         if difficulty:
             db.create_reading(section, chapter, group, reading)
-
+            print("Reading uploaded for", file)
 
 """
 Load reading from file
@@ -35,7 +36,9 @@ Load reading from file
 def load_reading(reading_path):
     with open(reading_path, 'r') as f:
         reading = f.readlines()
-    return ''.join(reading)
+    reading = ''.join(reading)
+    reading = reading.replace('\n', '\\n')
+    return reading
 
 
 if __name__ == '__main__':
@@ -52,3 +55,4 @@ if __name__ == '__main__':
 
     upload_reading(args.section, args.chapter, args.reading_path, args.group)
     # python upload_reading.py --reading_path content_generation/content/reading/2fa --section two_factor_authentication --chapter introduction_to_2fa
+    # python upload_reading.py --reading_path content_generation/content/reading/online_privacy/introduction_to_online_privacy --section online_privacy --chapter introduction_to_online_privacy
